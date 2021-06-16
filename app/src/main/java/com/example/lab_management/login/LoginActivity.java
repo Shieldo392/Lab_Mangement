fc7037b1396f525de0e772645304a585b85eded6
LapTop_TranDat <ADMIN@DESKTOP-SUKQLAU>
2021-06-16 22:02:36 +0700

    create XML Login

---
+++ app/src/main/java/com/example/lab_management/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab_management.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab_management.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
